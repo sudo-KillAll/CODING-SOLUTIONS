d25fe3ed9c06f7a824a67d9807215cac10ac4687
sudo1729 <tripathiakhilesh98163@gmail.com>
2020-10-31 22:02:18 +0530

    Modified the solution of 1st problem

--- Interviewbit/2_single_elements.cpp
+++ Interviewbit/2_single_elements.cpp
@@ -1,3 +1,27 @@
+/*Problem : 
+    Given An array of n integers , every integer in array occurs twice except 2 elements which
+    occurs only once,Find these single occuring numbers.
+*/
+
+/*
+Approach:
+    1) First find the xor of all Numbers of array, Let x;
+    2) find position any set bit of x , for sake of simplicity we took Rightmost set bit.Let y;
+    3) Initialize num1 and num2 as zero.
+    4) Now traverse the array and check yth bit of A[i],
+    if(A[i]==1)
+        num1=num1^A[i]
+    if(A[i]==0)
+        num2=num2^A[i];
+
+    "The idea is that if we do this way then array will be partitioned into 2 parts ,
+    where 1 part will contain the numbers with yth bit set,and 2nd part numbers will have yth bit
+    not set.This way if we xor each parts independently then we'll get answer for each part."
+
+    5) num1 and num2 are required answer.
+*/
+
+
 #include<bits/stdc++.h>
 
 #define ll long long int
