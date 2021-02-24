4b86b84886dce84cd5ebd5561ba581c52b62ce78
sudo1729 <tripathiakhilesh98163@gmail.com>
2021-02-24 19:46:01 +0530

    Added Spoj Problems

---
+++ SPOJ/Philosophers_Stone.cpp
@@ -0,0 +1,84 @@
+/*
+``````````````````$$$$$
+`````````````$$$$$$´´´´$$$$$$
+``````````$$$´´´´´´´´´´´´´´´´$$$
+````````$`$$´´´´´´´´´´´´´´´´´´´´$$
+```````$´$$$´´´´´´´´´´´´´´´´´´´´´$$$$
+`````$´´$$$$´´´´´´´´´´´´´´´´´´´´´´´´´´$
+````$´´$$$$$´´´´´´´´´´$$$$$$$´´´´´´´´´$$
+```$´´´$$$$$$$´´´$$$$$$$$$$$$$$$$$´´´´´$$
+``$´´´´$$$$$$$$$$$$$$$$$$$$$$$$$$$$$´´´´$$
+`$´´´´´´$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$´´$
+`$´´´´´´$$$$$$$$$$$´´´´$$$$$$$$$$$$$$$$$$$
+$´´´´´´´´$$$$$$$$´´´´´´´´$$$$$$$´´´´´´´´$$
+$´´´´´´´´´$$$$$$´´´´´´´´´´$$$$´´´´´´´´´´´$
+`$´´´´´´´´´$$$$$´´´´´´´´´´$$$´´´´´´´´´´´$
+`$´´´´´´´´´´$$$$$´´´´´´´´$$$$´´´´´´´´´´´$
+`$´´´´´´´´´´´$$$$$$´´´´$$$$$´´´´´´´´´´´$$
+``$´´´´´´´´´´´$$$$$$$$$$$$$$´´´´´´´´´´´$
+``$$´´´´´´´´´´´´$$$$$$$$$$$$´´´´´´´´´´$$
+```$$´´´´´´´´´´´´$$$$$$$$$$´´´´´´´´´´$$
+````$´´´´´´´´´´´´$$$$$$$$$´´´´´´´´´´´$
+`````$´´´´´´´´´´´$$$$$$$$´´´´´´´´´´´$
+``````$$´´´´´´´´´$$$$$$´´´´´´´´´´´$$
+````````$$´´´´´´$$$$$´´´´´´´´´´´$$
+``````````$$$´$$$$´´´´´´´´´´´$$$
+`````````````$$$$$´´´´´´$$$$$
+``````````````````$$$$$$
+*/
+#include<bits/stdc++.h>
+ 
+#define ll long long int
+#define pb push_back
+#define F first
+#define S second
+#define vi vector<ll>
+#define vs vector<string>
+#define input(v,n) for(ll VAL=0;VAL<n;VAL++){ll VALUE;cin>>VALUE;v.pb(VALUE);}
+#define mi map<ll,ll>
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define mi map<ll,ll>
+#define print(v) for(ll printing=0;printing<v.size();printing++){cout<<v[printing]<<' ';}
+#define TestCase ll testcase;cin>>testcase;while(testcase--)
+#define bin(n) bitset<32>(n).to_string();
+#define maxv(v) *max_element(v.begin(),v.end())
+#define minv(v) *min_element(v.begin(),v.end())
+#define decimal(s) stoll(s,nullptr,2)
+#define rmLead(str) str.erase(0, min(str.find_first_not_of('0'), str.size()-1));
+using namespace std;
+
+void solve(){
+    ll n,m;cin>>n>>m;
+    ll dp[n][m];
+    FOR(i,0,n){
+        FOR(j,0,m){
+            cin>>dp[i][j];
+        }
+    }
+
+
+    FOR(i,1,n){
+        FOR(j,0,m){
+            ll val=dp[i][j];
+            if(j-1>=0){dp[i][j]=max(dp[i][j],dp[i-1][j-1]+val);}
+            if(j+1<m){dp[i][j]=max(dp[i][j],dp[i-1][j+1]+val);}
+            dp[i][j]=max(dp[i][j],dp[i-1][j]+val);
+        }
+    }
+    ll ans=-1;
+    FOR(i,0,m){
+        ans=max(ans,dp[n-1][i]);
+    }
+    cout<<ans<<"\n";
+
+    
+}
+ 
+int main(){
+ios_base::sync_with_stdio(false); 
+cin.tie(NULL);
+cout.tie(NULL);
+TestCase{
+    solve();
+}
+} 
