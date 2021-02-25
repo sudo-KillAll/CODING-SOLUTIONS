fd7844d3e69ce65c50429f9e6eafc740eaa0fcec
sudo1729 <tripathiakhilesh98163@gmail.com>
2021-02-25 20:37:12 +0530

    Spoj Problems

---
+++ SPOJ/Build_a_Fence.py
@@ -0,0 +1,7 @@
+pi = 3.141592654
+while(True):
+    n=int(input())
+    if(n==0):
+        break
+    else:
+        print(round((n*n)/(2*pi),2))
---
+++ SPOJ/Counting_Triangles.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long int t;cin>>t;
+while(t--){
+    long long int n;cin>>n;
+    long long int ans=(2*(n*(n-1)))+1;
+    cout<<ans<<"\n";
+}
+} 
